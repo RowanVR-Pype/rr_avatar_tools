5778aed23ec37a6ee9c5511bc1e13a6b3e97c7f5
Joshua Skelton <joshua@recroom.com>
2025-08-15 12:38:01 -0700

    v1.1.0

--- rr_avatar_tools/__init__.py
+++ rr_avatar_tools/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Rec Room Avatar Tools",
     "author": "Joshua Skelton",
-    "version": (1, 0, "2-dev"),
+    "version": (1, 1, 0),
     "blender": (4, 2, 0),
     "location": "",
     "description": "",
